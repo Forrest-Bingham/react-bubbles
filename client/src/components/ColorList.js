d00a766ec1307fbe0cb5395f167cb32d8104a835
Forrest Bingham <bingham_5@live.com>
2019-12-20 10:37:28 -0700

    Finished MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,13 +19,28 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+   // console.log("colorList", colorList);
+    console.log("colors", colorToEdit);
+    axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then( res => {
+      console.log("new color", res.data);
+     
+    })
+    .catch(err => console.log(err));
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is it saved right now?
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    console.log("color", color)
+    axiosWithAuth().delete(`/colors/${color.id}`)
+    .then(res => {
+      console.log("Color Deleted:", res)
+
+    })
+    .catch(err => console.log(err));
   };
 
   return (
@@ -81,7 +97,7 @@ const ColorList = ({ colors, updateColors }) => {
         </form>
       )}
       <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      {/* stretch - build another form here to add a color */} 
     </div>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+import {axiosWithAuth} from "../utils/axiosWithAuth";
 
-const Login = () => {
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({username:"",password:""})
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handleChange = e => {
+    setCredentials({...credentials, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('credentials', credentials);
+    setIsFetching(true);
+    axiosWithAuth()
+        .post('/login', credentials)
+        .then(res => {
+          localStorage.setItem("token", res.data.payload);
+          props.history.push("/BubblePage")
+        })
+        .catch(err => console.log(err));
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input 
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={credentials.username}
+                onChange={handleChange}
+                />
+          <input 
+                type="text"
+                name="password"
+                placeholder="Password"
+                value={credentials.password}
+                onChange={handleChange}
+                />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </>
   );
 };
